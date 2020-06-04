13f5b768b26f10209cab591c546d119c76cfd778
Rohit Singh <59027452+Rohit130508@users.noreply.github.com>
2020-06-04 18:46:06 +0530

    Livingtheapp

--- app/src/main/java/com/livingtheapp/user/fragment/OffersFrag.java
+++ app/src/main/java/com/livingtheapp/user/fragment/OffersFrag.java
@@ -2,7 +2,10 @@ package com.livingtheapp.user.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,6 +23,7 @@ public class OffersFrag  extends Fragment {
     private static final String TAG = "SpeedDial";
 
 
+    RecyclerView rvList;
     public OffersFrag() {
         // Required empty public constructor
     }
@@ -33,13 +37,45 @@ public class OffersFrag  extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_offers, container, false);
-
+        rvList = root.findViewById(R.id.rvList);
+        rvList.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL,false));
+        OfferAdapter adapter = new OfferAdapter();
+        rvList.setAdapter(adapter);
         return root;
+    }
+    class OfferAdapter extends RecyclerView.Adapter<OfferAdapter.ViewHolder>
+    {
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_offer,
+                    parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 3;
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+            }
+        }
     }
 }
